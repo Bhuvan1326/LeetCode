d7dffe0ca56820cde80e81f1d05404803fceb804
Bhuvan1326 <bhuvanpatil1313@gmail.com>
2026-04-29 23:42:20 +0530

    Add

---
+++ Leaders-in-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> leaders(vector<int>& nums) {
+        int n=nums.size();
+        int maxi=INT_MIN;
+        vector<int> ans;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(nums[i]>=maxi)
+            {
+                ans.push_back(nums[i]);
+                maxi=nums[i];
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
