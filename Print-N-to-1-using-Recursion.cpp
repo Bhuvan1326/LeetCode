73e29ec0f644bfe8ef63264376a173dbd52c0530
Bhuvan1326 <bhuvanpatil1313@gmail.com>
2026-08-13 18:42:02 +0530

    Add

---
+++ Print-N-to-1-using-Recursion.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void fun(int i,int n)
+{ 
+    if(i>n) return;
+    fun(i+1,n);
+    cout<<i<<endl;
+}
+int main()
+{
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    fun(1,n);
+    return 0;
+}
